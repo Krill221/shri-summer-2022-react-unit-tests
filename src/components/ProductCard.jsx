78d9330d77bdbbdeb90122de14bca658d18b4876
Еропкин Кирилл <krill2211@yandex.ru>
2022-10-20 00:42:37 +0300

    -v 0.1 done

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,4 +1,84 @@
-export const ProductCard = () => {
-    // Реализуйте компонент
-    return <div data-testid="product-card" />
+import classnames from 'classnames'
+import styles from './ProductCard.module.css'
+import Icon from './Icon'
+
+export const ProductCard = ({
+    title,
+    imageUrl,
+    isHit,
+    isSale,
+    isFavorite,
+    currentPrice,
+    oldPrice,
+    flowersCount,
+    bouquetHeight,
+    bouquetWidth,
+}) => {
+
+    const card_title = title
+
+    const defImageUrl = 'https://en.wikipedia.org/wiki/Image#/media/File:Image_created_with_a_mobile_phone.png'
+    const card_imageUrl = (imageUrl !== undefined && imageUrl !== '') ? imageUrl : defImageUrl
+
+    const card_isHit = isHit
+    const card_isSale = isSale
+    const card_isFavorite = isFavorite
+    const card_currentPrice = currentPrice
+    const card_oldPrice = oldPrice
+    const card_flowersCount = flowersCount
+    const card_bouquetHeight = bouquetHeight
+    const card_bouquetWidth = bouquetWidth
+
+    const showOldPrice = (card_oldPrice !== undefined && card_oldPrice !== 0 && card_oldPrice !== '' && card_oldPrice !== '0')
+
+    return <div data-testid="product-card" className={classnames(styles.card)} >
+        <div data-testid="product-image" className={classnames(styles.image)} style={{ backgroundImage: `url(${card_imageUrl})` }}>
+
+            {card_isHit && <div className={classnames(styles.hit)}>Хит</div>}
+
+            {card_isSale && <div className={classnames(styles.sale)}>Скидка</div>}
+
+            <div className={classnames(styles.fav)}>
+                {
+                    card_isFavorite ?
+                        <Icon iconType={'infav'} />
+                        :
+                        <Icon iconType={'outfav'} />
+                }
+            </div>
+        </div>
+        <div className={classnames(styles.info)}>
+            <div className={classnames(styles.title)}>{card_title}</div>
+            <div className={classnames(styles.price)}>
+                <div className={classnames(card_isSale ? styles.currentPriceRed : styles.currentPrice)}>{card_currentPrice} ₽</div>
+                {
+                    showOldPrice && <div data-testid="product-old-price" className={classnames(styles.oldPrice)}>{card_oldPrice} ₽</div>
+                }
+            </div>
+            <div className={classnames(styles.params)}>
+                {
+                    (card_flowersCount !== 0 && card_flowersCount !== undefined) && <div data-testid="product-count" className={classnames(styles.number)}>
+                        <Icon iconType={'number'} />
+                        {card_flowersCount} шт.
+                    </div>
+                }
+                {
+                    (card_bouquetWidth !== 0 && card_bouquetWidth !== undefined) && <div data-testid="product-width" className={classnames(styles.width)}>
+                        <Icon iconType={'width'} />
+                        {card_bouquetWidth} см
+                    </div>
+                }
+                {
+                    (card_bouquetHeight !== 0 && card_bouquetHeight !== undefined) && <div data-testid="product-height" className={classnames(styles.height)}>
+                        <Icon iconType={'height'} />
+                        {card_bouquetHeight} см
+                    </div>
+                }
+            </div>
+        </div>
+        <div className={classnames(styles.actions)}>
+            <button data-testid="product-cart" disabled={(card_flowersCount === 0)} onClick={() => alert('cart')} className={classnames(styles.cartBtn)}>В корзину</button>
+            <button data-testid="product-buy" disabled={(card_flowersCount === 0)} onClick={() => alert('buy')} className={classnames(styles.buyBtn)}>Купить сразу</button>
+        </div>
+    </div>
 }
--- src/components/ProductCard.test.jsx
+++ src/components/ProductCard.test.jsx
@@ -1,5 +1,287 @@
+
+import { render, screen } from "@testing-library/react";
+import { ProductCard } from './ProductCard'
+import { getBouquetItem } from '../__mocks__/bouquetItem'
+
+//const {container, getByTestId} =  render(application)
+//const hit = getByTestId('product-hit')
+//screen.logTestingPlaygroundURL()
+
 describe('Компонент «Карточка товара»', () => {
-    it('Simple empty test', () => {
-        expect(true).toBeTruthy();
-    });
+
+
+    // hit and sale
+    it('Отображает "Хит" элемент, если он есть', () => {
+        const bouquet = getBouquetItem()
+        bouquet.isHit = true
+
+        const application = <ProductCard {...bouquet} />
+        render(application)
+
+        expect(screen.queryByText('Хит')).toBeInTheDocument()
+    })
+
+    it('Не отображает "Хит" элемент, если его нет', () => {
+        const bouquet = getBouquetItem()
+        bouquet.isHit = false
+
+        const application = <ProductCard {...bouquet} />
+        render(application)
+
+        expect(screen.queryByText('Хит')).not.toBeInTheDocument()
+    })
+
+    it('Отображает "Скидка" элемент, если он есть', () => {
+        const bouquet = getBouquetItem()
+        bouquet.isSale = true
+
+        const application = <ProductCard {...bouquet} />
+        render(application)
+
+        expect(screen.queryByText('Скидка')).toBeInTheDocument()
+    })
+
+    it('Не отображает "Скидка" элемент, если его нет', () => {
+        const bouquet = getBouquetItem()
+        bouquet.isSale = false
+
+        const application = <ProductCard {...bouquet} />
+        render(application)
+
+        expect(screen.queryByText('Скидка')).not.toBeInTheDocument()
+    })
+
+    it('Отображает "Хит" и "Скидка" элемент, если они есть', () => {
+        const bouquet = getBouquetItem()
+        bouquet.isSale = true
+        bouquet.isHit = true
+
+        const application = <ProductCard {...bouquet} />
+        render(application)
+
+        expect(screen.queryByText('Хит')).toBeInTheDocument()
+        expect(screen.queryByText('Скидка')).toBeInTheDocument()
+    })
+
+    it('Не отображает "Хит" и "Скидка" элемент, если их нет', () => {
+        const bouquet = getBouquetItem()
+        bouquet.isSale = false
+        bouquet.isHit = false
+
+        const application = <ProductCard {...bouquet} />
+        render(application)
+
+        expect(screen.queryByText('Хит')).not.toBeInTheDocument()
+        expect(screen.queryByText('Скидка')).not.toBeInTheDocument()
+    })
+
+
+
+    // price
+    it('Отображает "Старую сцену" элемент, если старая цена есть', () => {
+        const bouquet = getBouquetItem()
+        bouquet.oldPrice = 1000
+
+        const application = <ProductCard {...bouquet} />
+        const { getByTestId } = render(application)
+
+        const oldPrice = getByTestId('product-old-price')
+
+        expect(oldPrice).toBeInTheDocument()
+    })
+
+    it('Не отображает "Старую сцену" элемент, если старая цена undefined', () => {
+        const bouquet = getBouquetItem()
+        bouquet.oldPrice = undefined
+
+        const application = <ProductCard {...bouquet} />
+        render(application)
+
+        const oldPrice = screen.queryByTestId('product-old-price')
+
+        expect(oldPrice).not.toBeInTheDocument()
+    })
+
+    it('Не отображает "Старую сцену" элемент, если старая цена 0', () => {
+        const bouquet = getBouquetItem()
+        bouquet.oldPrice = 0
+
+        const application = <ProductCard {...bouquet} />
+        render(application)
+
+        const oldPrice = screen.queryByTestId('product-old-price')
+
+        expect(oldPrice).not.toBeInTheDocument()
+    })
+
+    it('Не отображает "Старую сцену" элемент, если старая цена ""', () => {
+        const bouquet = getBouquetItem()
+        bouquet.oldPrice = ''
+
+        const application = <ProductCard {...bouquet} />
+        render(application)
+
+        const oldPrice = screen.queryByTestId('product-old-price')
+
+        expect(oldPrice).not.toBeInTheDocument()
+    })
+
+    it('Не отображает "Старую сцену" элемент, если старая цена "0"', () => {
+        const bouquet = getBouquetItem()
+        bouquet.oldPrice = '0'
+
+        const application = <ProductCard {...bouquet} />
+        render(application)
+
+        const oldPrice = screen.queryByTestId('product-old-price')
+
+        expect(oldPrice).not.toBeInTheDocument()
+    })
+
+
+    // fav
+    // price
+    it('Активный компонент "избранное", если выбран', () => {
+        const bouquet = getBouquetItem()
+        bouquet.isFavorite = true
+
+        const application = <ProductCard {...bouquet} />
+        render(application)
+
+        const infav = screen.queryByTestId('product-icon-infav')
+        const outfav = screen.queryByTestId('product-icon-outfav')
+
+        //screen.logTestingPlaygroundURL()
+
+        expect(infav).toBeInTheDocument()
+        expect(outfav).not.toBeInTheDocument()
+    })
+
+    it('Не активный компонент "избранное", если не выбран', () => {
+        const bouquet = getBouquetItem()
+        bouquet.isFavorite = false
+
+        const application = <ProductCard {...bouquet} />
+        render(application)
+
+        const infav = screen.queryByTestId('product-icon-infav')
+        const outfav = screen.queryByTestId('product-icon-outfav')
+
+        expect(infav).not.toBeInTheDocument()
+        expect(outfav).toBeInTheDocument()
+    })
+
+    /// image
+    it('Show default pic, pic url is undefined', () => {
+        const bouquet = getBouquetItem()
+        bouquet.imageUrl = undefined
+
+        const application = <ProductCard {...bouquet} />
+        render(application)
+
+        const image = screen.queryByTestId('product-image')
+        const defImageUrl = 'https://en.wikipedia.org/wiki/Image#/media/File:Image_created_with_a_mobile_phone.png'
+
+        expect(image).toHaveStyle(`background-image: url(${defImageUrl})`)
+    })
+    it('Show default pic, pic url is ""', () => {
+        const bouquet = getBouquetItem()
+        bouquet.imageUrl = ''
+
+        const application = <ProductCard {...bouquet} />
+        render(application)
+
+        const image = screen.queryByTestId('product-image')
+        const defImageUrl = 'https://en.wikipedia.org/wiki/Image#/media/File:Image_created_with_a_mobile_phone.png'
+
+        expect(image).toHaveStyle(`background-image: url(${defImageUrl})`)
+    })
+
+
+    // buttons
+    it('Disabled buy and cart buttons if flowers count is 0', () => {
+        const bouquet = getBouquetItem()
+        bouquet.flowersCount = 0
+
+        const application = <ProductCard {...bouquet} />
+        render(application)
+
+        const buy = screen.queryByTestId('product-buy')
+        const cart = screen.queryByTestId('product-cart')
+
+        expect(buy).toBeDisabled()
+        expect(cart).toBeDisabled()
+    })
+
+    // count
+    it('Not show count element if flowers count is 0', () => {
+        const bouquet = getBouquetItem()
+        bouquet.flowersCount = 0
+
+        const application = <ProductCard {...bouquet} />
+        render(application)
+
+        const count = screen.queryByTestId('product-count')
+
+        expect(count).not.toBeInTheDocument()
+    })
+    it('Not show count element if flowers count is undefined', () => {
+        const bouquet = getBouquetItem()
+        bouquet.flowersCount = undefined
+
+        const application = <ProductCard {...bouquet} />
+        render(application)
+
+        const count = screen.queryByTestId('product-count')
+
+        expect(count).not.toBeInTheDocument()
+    })
+
+    // width
+    it('Not show width element if flowers width is 0', () => {
+        const bouquet = getBouquetItem()
+        bouquet.bouquetWidth = 0
+
+        const application = <ProductCard {...bouquet} />
+        render(application)
+
+        const width = screen.queryByTestId('product-width')
+
+        expect(width).not.toBeInTheDocument()
+    })
+    it('Not show width element if flowers width is undefined', () => {
+        const bouquet = getBouquetItem()
+        bouquet.bouquetWidth = undefined
+
+        const application = <ProductCard {...bouquet} />
+        render(application)
+
+        const width = screen.queryByTestId('product-width')
+
+        expect(width).not.toBeInTheDocument()
+    })
+
+    // height
+    it('Not show height element if flowers height is 0', () => {
+        const bouquet = getBouquetItem()
+        bouquet.bouquetHeight = 0
+
+        const application = <ProductCard {...bouquet} />
+        render(application)
+
+        const height = screen.queryByTestId('product-height')
+
+        expect(height).not.toBeInTheDocument()
+    })
+    it('Not show height element if flowers height is undefined', () => {
+        const bouquet = getBouquetItem()
+        bouquet.bouquetHeight = undefined
+
+        const application = <ProductCard {...bouquet} />
+        render(application)
+
+        const height = screen.queryByTestId('product-height')
+
+        expect(height).not.toBeInTheDocument()
+    })
 });
